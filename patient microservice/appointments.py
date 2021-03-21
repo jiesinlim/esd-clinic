6052381637271041301dea944bb2f8b060b41994
wqteoh <wqteoh.2019@smu.edu.sg>
2021-03-21 18:18:09 +0800

    add status='booked'

--- patient microservice/appointments.py
+++ patient microservice/appointments.py
@@ -62,7 +62,7 @@ class Appointments(db.Model):
     appointment_time = db.Column(db.appointment_time, nullable=False)
     did = db.Column(db.Integer, nullable=True)
     doctor_name = db.Column(db.VARCHAR(15), nullable=True)
-    status = db.Column(db.VARCHAR(10), nullable=True)
+    status = db.Column(db.VARCHAR(10), nullable=False)
     room_no = db.Column(db.VARCHAR(10), nullable=True)
 
     def __init__(self, aid, pid, did, doctor_name, appointment_date, appointment_time, status, room_no):
@@ -83,7 +83,6 @@ class Appointments(db.Model):
 
 # Add new appointment
 # [POST] 
-# /appointment 
 @app.route("/appointment", methods=['POST'])
 def add_new_appointment(nric, appointment_date, appointment_time):
     # !!!!! need to check if the inputs are correct?
@@ -94,15 +93,16 @@ def add_new_appointment(nric, appointment_date, appointment_time):
                 "data": {
                     "nric": nric,
                     "appointment_date": appointment_date,
-                    "appointment_time" : appointment_time
+                    "appointment_time" : appointment_time,
                 },
                 "message": "Appointment already exists."
             }
         ), 400
 
     data = request.get_json()
-    appointment = Appointments(nric, appointment_date, appointment_time, **data)
-
+    appointment = Appointments(nric, appointment_date, appointment_time, status='booked', **data)
+    #!!!!! check how to add status='booked'
+    
     try:
         db.session.add(appointment)
         db.session.commit()
@@ -128,7 +128,6 @@ def add_new_appointment(nric, appointment_date, appointment_time):
 		
 # Get appointment details
 # [GET] 
-# /appointment?status={booked/confirmed}
 @app.route("/appointment/<string:aid>")
 def get_appointment_details(aid):
     appointment = Appointment.query.filter_by(aid=aid).first()
@@ -148,7 +147,6 @@ def get_appointment_details(aid):
 
 # Change Date & Time of appointment 
 # [PUT] 
-# /appointment/{appointmentID}
 @app.route("/appointment/<string:aid>", methods=['PUT'])
 def change_appointment_details(aid):
     appointment = appointment.query.filter_by(aid=aid).first()
@@ -178,7 +176,6 @@ def change_appointment_details(aid):
 
 # Delete appointment details
 # [GET]
-# /appointment?status={booked/confirmed}
 @app.route("/appointment/<string:aid>", methods=['DELETE'])
 def delete_appointment_details(aid):
     appointment = Appointment.query.filter_by(aid=aid).first()
