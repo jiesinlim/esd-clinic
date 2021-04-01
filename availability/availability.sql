53932189d2c1a3bd7351eaa46c548ed2cfa23d04
marcsoh-smu <71428250+marcsoh-smu@users.noreply.github.com>
2021-04-01 23:13:40 +0800

    Availability.sql

---
+++ availability/availability.sql
@@ -0,0 +1,56 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Mar 18, 2021 at 05:18 PM
+-- Server version: 8.0.18
+-- PHP Version: 7.4.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `esd_clinic`
+--
+CREATE DATABASE IF NOT EXISTS `availability` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+USE `availability`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `availability`
+--
+
+DROP TABLE IF EXISTS `availability`;
+CREATE TABLE IF NOT EXISTS `availability` (
+  `aid` int(11) NOT NULL AUTO_INCREMENT,
+  `did` int(11) NOT NULL,
+  `doctor_name` char(26) NOT NULL,
+  `date` date NOT NULL,
+  `availability` varchar(1000) DEFAULT NULL,
+  CONSTRAINT `availability_pk` PRIMARY KEY (`aid`, `did`, `doctor_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `availability`
+--
+
+INSERT INTO `availability` (`aid`, `did`, `doctor_name`, `date`, `availability`) VALUES
+(1, 1, 'Dr. Marcus', '2021-03-21', '0900, 1000, 1100, 1300, 1400, 1500'),
+(2, 2, 'Dr. Alan', '2021-03-22', '1200, 1300, 1400, 1500, 1600, 1700'),
+(3, 3, 'Dr. Hong Seng', '2021-03-23', '1600, 1700, 1800, 1900, 2000, 2100'),
+(4, 4, 'Dr. Strange', '2021-03-24', '0900, 1000, 1500, 1800, 2000, 2100'),
+(5, 5, 'Dr. Oz', '2021-03-25', '1100, 1300, 1400, 1900, 2000, 2100'),
+(6, 6, 'Dr. Phil', '2021-03-26', '0800, 0900, 1000, 1100, 1200, 1300');
+COMMIT;
+-- --------------------------------------------------------
