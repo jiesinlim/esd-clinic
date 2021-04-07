544548a35097bc15c2997221f0216c7c0398b605
marcsoh-smu <71428250+marcsoh-smu@users.noreply.github.com>
2021-04-07 21:15:33 +0800

    docker-compose in one folder test

---
+++ docker-compose/appointment.sql
@@ -0,0 +1,53 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+08:00";
+
+--
+-- Database: `esd_clinic`
+--
+CREATE DATABASE IF NOT EXISTS `appointment` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+USE `appointment`;
+
+
+-- --------------------------------------------------------
+--
+-- Table structure for table `appointment`
+--
+
+DROP TABLE IF EXISTS `appointment`;
+CREATE TABLE IF NOT EXISTS `appointment` (
+  `appointment_id` int(5) NOT NULL AUTO_INCREMENT,
+  `NRIC` varchar(9) NOT NULL,
+  `patient_name` varchar(15) NOT NULL,
+  `gender` varchar(15) NOT NULL,
+  `contact_number` int(8) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `appointment_date` varchar(20) NOT NULL,
+  `appointment_time` varchar(9) NOT NULL,
+  `did` int(5) DEFAULT NULL,
+  `aid` int(11) DEFAULT NULL,
+  `doctor_name` varchar(15) DEFAULT NULL,
+  `status` varchar(10) NOT NULL DEFAULT 'booked',
+  `room_no` varchar(10) NULL,
+  PRIMARY KEY (`appointment_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `appointment`
+--
+
+INSERT INTO `appointment` (`appointment_id`,`NRIC`, `patient_name`,`gender`,`contact_number`,`email`, `appointment_date`, `appointment_time`, `did`,`aid`, `doctor_name`, `status`, `room_no`) VALUES
+(1, 'T0123456U', 'Leslie','M','82873618', 'leslie123@gmail.com', '2021-03-22', '1200', NULL , NULL , NULL, 'booked', NULL),
+(2, 'T1234567I', 'Reuben','M','83476123', 'reuben234@yahoo.com', '2021-03-21', '0900', NULL , NULL , NULL, 'booked', NULL),
+(3, 'S1234567J', 'Jasmine','F','98374986', 'jasmine345@hotmail.com', '2021-03-23', '1600', NULL , NULL , NULL, 'booked', NULL),
+(4, 'S9234567I', 'Amelia','F','81097192', 'amelia456@abc.com', '2021-03-24', '0900', NULL , NULL , NULL, 'booked', NULL),
+(5, 'S8745120I', 'Jack','M','98683274', 'jack567@xyz.com', '2021-03-25', '1100', NULL , NULL , NULL, 'booked', NULL);
+
+COMMIT;
+
+
+
+
+
---
+++ docker-compose/notify.py
@@ -0,0 +1,134 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+import os
+import sys
+from os import environ
+
+import requests
+from invokes import invoke_http
+
+import json
+import amqp_setup
+import pika
+
+
+app = Flask(__name__)
+CORS(app)
+
+#get next day appts
+appointment_URL = environ.get('appointment_URL') or "http://127.0.0.1:5005/appointment" 
+
+@app.route("/confirm/<string:date>", methods=['GET'])
+def displayMatchedAppts(date):
+    # Invoke the appointment microservice
+    print('\n-----Invoking appointments microservice-----')
+    if date:
+        try:
+        # do the actual work
+            result = getMatchedAppts(date)
+            return result, result["code"]
+
+        except Exception as e:
+            # Unexpected error in code
+            pass
+
+            return jsonify({
+                "code": 500,
+                "message": "appointment.py internal error"
+            }), 500
+
+    # if reached here, not a JSON request.
+    return jsonify({
+        "code": 400,
+        "message": "Invalid JSON input: " + str(request.get_data())
+    }), 400
+
+def getMatchedAppts(date):
+    print('\n-----Invoking appt microservice-----')
+    matchedAppts = invoke_http(appointment_URL + '/nextday/' + str(date), method='GET')
+    print('Matched Appts:', matchedAppts)
+    return matchedAppts
+
+@app.route("/confirm/confirmAppts", methods = ['PATCH'])
+def confirm_appointments():
+    if request.is_json:
+        try:
+            data = request.get_json()
+
+            # do the actual work
+            result = updateConfirmDetails(data)
+            return result, result["code"]
+
+        except Exception as e:
+            pass  # do nothing.
+
+        # if reached here, not a JSON request.
+        return jsonify({
+            "code": 400,
+            "message": "Invalid JSON input: " + str(request.get_data())
+        }), 400
+
+def updateConfirmDetails(data):
+    print(type(data))
+    #update patient appointment with assigned doctor id and name
+    # Invoke the appointment microservice
+        # do the actual work
+    #parse match into 6 separate values
+    patient_name = data['patient_name']
+    email = data['email']
+    appt_id = data['appointment_id']
+    appt_time = data['appointment_time']
+ 
+
+    # appt_id = data['appointment_id']
+    # patient_name = data['patient_name']
+    # doc_name = data['d_name']
+    # time = data['appt_time']
+    # appt_date = data['appointment_date']
+    # Invoke the appointment microservice
+
+    print('\n-----Invoking appointments microservice-----')
+
+    details = {
+            "NRIC": "",
+            "aid": "",
+            "appointment_date": "",
+            "appointment_id": appt_id,
+            "appointment_time": "",
+            "contact_number": "",
+            "did": "",
+            "doctor_name": "",
+            "email": "",
+            "gender": "",
+            "patient_name": "",
+            "room_no": "",
+            "status": "confirmed"
+            }
+
+    appt_details = json.dumps(details)
+
+    print(appt_details)
+    updateStatus = invoke_http(appointment_URL, method='PATCH', json=appt_details)
+    print('Update result:', updateStatus)
+
+    print('\n-----Invoking notification microservice-----')
+
+    notify_patient = {
+            "patient_name": patient_name,
+            "email": email,
+            "appointment_time": appt_time
+            }
+
+    notification_details = json.dumps(notify_patient)
+
+    print(notification_details)
+    #notification = invoke_http(notification_URL, method='PATCH', json=notification_details)
+    if(updateStatus["code"] in range(200, 300)):
+        amqp_setup.channel.basic_publish(exchange=amqp_setup.exchangename, routing_key="send.email", 
+        body=notification_details, properties=pika.BasicProperties(delivery_mode = 2)) 
+
+if __name__ == '__main__':
+    print("This is flask for " + os.path.basename(__file__) +
+        ": notify patient ...")
+    app.run(host='0.0.0.0', port=5006, debug=True)
